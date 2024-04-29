8957e4c7e42d0e279b258d2023f2e598f910ad45
Ehsan-Tanvir <ehsantanvir13@gmail.com>
2024-04-29 15:06:36 +0500

    Signup, Signin Page With Routes For Student Added

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import Signin from "./componenets/Signin";
+import Signup from "./componenets/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
