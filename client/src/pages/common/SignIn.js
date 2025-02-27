3abdb1064576872fe89f94a5de5ff5fe9e8201cc
Hassan Mamoon <mianhassan085@gmail.com>
2025-02-27 16:02:47 +0500

    updated folder structure

--- client/src/pages/common/SignIn.js
+++ client/src/pages/common/SignIn.js
@@ -11,7 +11,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../Context/AuthContext";
+import { AuthContext } from "../../Context/AuthContext";
 import Alert from "@mui/material/Alert";
 
 export default function SignInSide() {
